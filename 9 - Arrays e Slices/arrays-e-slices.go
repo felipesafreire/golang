96cda187540ed3c8d8fb12aad065979f342b2cc4
Felipe Sá Freire <felipe.freire@fabricadecodigos.com.br>
2021-03-08 13:33:24 -0300

    Arrays e Slices

---
+++ 9 - Arrays e Slices/arrays-e-slices.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Arrays e Slices")
+
+	var array1 [5]int
+	array1[0] = 100
+	fmt.Println(array1)
+
+	array2 := [5]string{"Posição 1", "Posição 2", "Posição 3", "Posição 4", "Posição 5"}
+	fmt.Println(array2)
+
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3)
+
+	slice := []int{10, 11, 13, 14, 15}
+	fmt.Println(slice)
+
+	slice = append(slice, 16)
+	fmt.Println(slice)
+
+	slice2 := array2[1:3]
+	fmt.Println(slice2)
+
+	array2[1] = "Posição Alterada"
+	fmt.Println(slice2)
+
+	// Arrays Internos
+	fmt.Println("---------------")
+	slice3 := make([]float32, 10, 15)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // length
+	fmt.Println(cap(slice3)) // capacidade
+
+}
