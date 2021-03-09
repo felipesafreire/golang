9aae24ad1a77efd6e18f08bc0dd3ac80118865ea
Felipe Sá Freire <felipe.freire@fabricadecodigos.com.br>
2021-03-09 13:44:55 -0300

    Switch

---
+++ 12 - Switch/switch.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda-feira"
+	case 3:
+		return "Terça-feira"
+	case 4:
+		return "Quarta-feira"
+	case 5:
+		return "Quinta-feira"
+	case 6:
+		return "Sexta-feira"
+	case 7:
+		return "Sábado"
+	default:
+		return "Número Inválido"
+	}
+}
+
+func diaDaSemana2(numero int) string {
+
+	var diaDaSemana string
+
+	switch {
+	case numero == 1:
+		diaDaSemana = "Domingo"
+		fallthrough
+	case numero == 2:
+		diaDaSemana = "Segunda-feira"
+	case numero == 3:
+		diaDaSemana = "Terça-feira"
+	case numero == 4:
+		diaDaSemana = "Quarta-feira"
+	case numero == 5:
+		diaDaSemana = "Quinta-feira"
+	case numero == 6:
+		diaDaSemana = "Sexta-feira"
+	case numero == 7:
+		diaDaSemana = "Sábado"
+	default:
+		diaDaSemana = "Número Inválido"
+	}
+
+	return diaDaSemana
+}
+
+func main() {
+
+	fmt.Println("Switch")
+
+	dia := diaDaSemana(200)
+	fmt.Println(dia)
+
+	fmt.Println("----------")
+	dia2 := diaDaSemana2(1)
+	fmt.Println(dia2)
+
+}
